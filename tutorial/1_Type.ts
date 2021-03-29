9d1c1168f8be8a3d9843c1a27822efd882170601
summer-kim <48466707+summer-kim@users.noreply.github.com>
2021-03-30 02:29:18 +0900

    understand Type of TS file

---
+++ tutorial/1_Type.ts
@@ -0,0 +1,23 @@
+//TS string
+let str: string = 'summer';
+
+//TS number
+let num: number = 3;
+
+//TS array
+let arr: Array<number> = [1, 2, 3];
+let flower: Array<string> = ['rose', 'sunflower', 'cosmos'];
+//💣💥  let flower: Array<string> = ['rose', 'sunflower', 10];
+let lotto: number[] = [22, 46, 7, 31, 12];
+//💣💥  let lotto: number[] = [22, 46, 7, 31, 'summer']
+
+//TS tuple
+let tup: [number, string] = [3, 'dog'];
+//💣💥  let tup: [number, string] = [3, 4]
+
+//TS object
+let obj: object = {};
+let summer: { name: string; age: number } = { name: 'summer', age: 5 };
+
+//TS boolean
+let bool: boolean = true;
