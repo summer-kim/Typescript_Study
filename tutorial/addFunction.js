9fa391f2f84ff019c72fd5b3edc32f8ccb21f58b
summer-kim <48466707+summer-kim@users.noreply.github.com>
2021-03-30 01:51:39 +0900

    how to make JS work like TS

---
+++ tutorial/addFunction.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * @param {number} a first variable
+ * @param {number} b second variable
+ * @returns {number}
+ */
+
+//@ts-check
+function add(a, b) {
+  return a + b;
+}
+
+//const value = add(10, '20');
+const value = add(10, 20);
+console.log(value);
---
+++ tutorial/addFunction.ts
@@ -0,0 +1,7 @@
+function addTS(a: number, b: number) {
+  return a + b;
+}
+
+//const valueTS = addTS(10, '20');
+const valueTS = addTS(10, 20);
+console.log(valueTS);
