1d59f617eaf7dcd410df7b374c2f72a60ca9f560
summer-kim <48466707+summer-kim@users.noreply.github.com>
2021-03-31 20:37:12 +0900

    learned about "generic limit, extends"

--- 1_tutorial/6_Generic.ts
+++ 1_tutorial/6_Generic.ts
@@ -16,3 +16,26 @@ interface Vegetable<T>{
     color : T
 }
 const eggPlant: Vegetable<string> = {name:'eggPlant', color:'purple'}
+
+//Limit the type of generic
+function logVegetable1<T>(value: T[]){
+    const length = value.length
+    //If you declare just "T"
+    //unable to use .length api because type is implicit
+    return value.filter(vege=> !vege)
+}
+
+//Extends generic to other type already declared
+function logVegetable2<T extends Vegetable<number>>(value: T){
+    value.color = value.color + 1
+    return value
+}
+logVegetable2({ name:'cucumber', color: 33333})
+
+//Limit the type of generic - Keyof
+function logVegetable3<T extends keyof Vegetable<number>>(value: T){
+    return value
+}
+logVegetable3('color')
+logVegetable3('name')
+    //💣💥logVegetable3(3)
